87c0770df9128bb8612dc00fc06494a29011cd76
Sinch20 <83527617+Sinch20@users.noreply.github.com>
2022-09-18 11:31:39 +0530

    dyr

--- pro1/wsgi.py
+++ pro1/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
+from wsgiref.util import application_uri
 import whitenoise 
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise import DjangoWhiteNoise 
+application = DjangoWhiteNoise(application_uri)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pro1.settings')
 
