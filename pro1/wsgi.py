3ee3e86978d0a992945eb6fd0220d28bfff8b90c
Sinch20 <83527617+Sinch20@users.noreply.github.com>
2022-09-18 15:06:08 +0530

    llk

--- pro1/wsgi.py
+++ pro1/wsgi.py
@@ -23,6 +23,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pro1.settings')
 
 application = get_wsgi_application()
