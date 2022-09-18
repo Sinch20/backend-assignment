3fb62ca52a2846f913391156e84a64af92199bbf
Sinch20 <83527617+Sinch20@users.noreply.github.com>
2022-09-18 11:28:00 +0530

    lknkln

--- pro1/settings.py
+++ pro1/settings.py
@@ -81,17 +81,23 @@ WSGI_APPLICATION = 'pro1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'pro1',
+#         'HOST': 'localhost',
+#         'PORT': 3306,
+#         'USER': 'root',
+#         'PASSWORD': 'Sanj@2001',
+#     }
+# }
+ 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'pro1',
-        'HOST': 'localhost',
-        'PORT': 3306,
-        'USER': 'root',
-        'PASSWORD': 'Sanj@2001',
+    "default": {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
- 
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
