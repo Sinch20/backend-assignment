d33f134640294798b9994b026d15d5512a98e54b
Sinch20 <83527617+Sinch20@users.noreply.github.com>
2022-09-18 13:16:07 +0530

    Update wsgi.py

--- pro1/wsgi.py
+++ pro1/wsgi.py
@@ -1,20 +1,28 @@
-"""
-WSGI config for pro1 project.
+# """
+# WSGI config for pro1 project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+# It exposes the WSGI callable as a module-level variable named ``application``.
 
-For more information on this file, see
-https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
-"""
+# For more information on this file, see
+# https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+# """
+
+# import os
+# from wsgiref.util import application_uri
+# import whitenoise 
+
+# from django.core.wsgi import get_wsgi_application
+# from whitenoise import DjangoWhiteNoise 
+# application = DjangoWhiteNoise(application_uri)
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pro1.settings')
+
+# application = get_wsgi_application()
 
 import os
-from wsgiref.util import application_uri
-import whitenoise 
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise import DjangoWhiteNoise 
-application = DjangoWhiteNoise(application_uri)
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pro1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Project.settings')
 
 application = get_wsgi_application()
